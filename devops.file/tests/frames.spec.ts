c7a02cc540cf007aa87552e385b425aed083c1d3
manojmj192 <n.manojmj192@gmail.com>
2024-11-25 06:28:49 +0530

    dockerchanges

--- devops.file/tests/frames.spec.ts
+++ devops.file/tests/frames.spec.ts
@@ -1,11 +1,24 @@
 import { chromium, Page } from "playwright-core";
 import test from "playwright/test";
+import { devices } from "@playwright/test";
+
+
+// setting the responsive for the viewport
+// test.use({
+
+//   browserName :'chromium',
+//   ...devices['iPhone 15'] 
+
+// })
 
 test("verify the frames", async () => {
   const browser = await chromium.launch();
   const context = await browser.newContext();
   const page = await context.newPage();
 
+  
+  await page.context().tracing.start({screenshots:true,snapshots:true})
+  await page.setViewportSize({height:1080,width:1980})
   await page.goto("https://letcode.in/dropable");
 
   const source = page.locator('div[id="draggable"]');
@@ -15,4 +28,7 @@ test("verify the frames", async () => {
   await source.dragTo(desti);
 
   await page.waitForTimeout(20000);
+
+
+  await page.context().tracing.stop({path:'trace.zip'})
 });
